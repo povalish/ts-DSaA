a9962e74594d3b2d4f214797ee8a1deef602302b
Nikita Povalishev <np@skl.vc>
2023-04-19 00:08:45 +0300

    Add comments
    - search algorythms

--- src/algorithms/searching/binary.ts
+++ src/algorithms/searching/binary.ts
@@ -1,4 +1,3 @@
-
 /**
  * Binary search
  * Complexity: O(log(n))
--- src/algorithms/searching/linear.ts
+++ src/algorithms/searching/linear.ts
@@ -1,3 +1,9 @@
+
+/**
+ * Linear search
+ * Complexity: O(n)
+ * Space complexity: 0(1)
+ */
 export const linearNative = (array: number[], target: number): number | null => {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === target) {
@@ -8,6 +14,11 @@ export const linearNative = (array: number[], target: number): number | null => 
   return null;
 };
 
+/**
+ * Linear search
+ * Complexity: O(n)
+ * Space complexity: 0(1)
+ */
 export const linearJS = (array: number[], target: number): number | null => {
   const found = array.find((v) => v === target);
   return found || null;
